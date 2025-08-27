63fb8c9c164d09e5e480cb76af61085f00cf7922
Qurto108 <darifonova108@gmail.com>
2025-08-27 19:31:58 +0300

    Публикация

---
+++ appShop/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .models import Product
+from django.http import JsonResponse
+from django.views import View
+
+
+class PageHome(View):
+    def get(self, request):
+        return render(request, 'home.html')
+
+
+class SearchProduct(View):
+    def get(self, request):
+        query = request.GET.get('q', '')
+        
+        search_product = Product.objects.filter(
+            title__iregex = query
+        ).values('title', 'price')
+        
+        return JsonResponse({
+            'succes': True,
+            'query': query,
+            'result': list(search_product),
+            'count':len(search_product)
+        }, status=200)
